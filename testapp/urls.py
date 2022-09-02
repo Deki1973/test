82945ab03d6c552524445cc9ff6ca201504603c8
Deki1973 <konidek37@gmail.com>
2022-09-03 01:28:59 +0200

    dfdsfdsfsdf

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('pocetna', views.pocetna, name="pocetna"),
+    path('/pocetna', views.pocetna, name="pocetna"),
     path('/onama', views.o_nama, name="o_nama"),
     path('/pocenta2',views.pocetna2, name="pocetna2"),
 ]
