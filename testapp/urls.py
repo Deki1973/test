0fbfa48b2d3d93a6a73578aa1767cd8a92e40486
Deki1973 <konidek37@gmail.com>
2022-09-03 01:15:16 +0200

    nova funckija

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.pocetna, name="pocetna"),
+    path('/onama', views.o_nama, name="o_nama"),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def pocetna(request):
     return HttpResponse("OVO JE POCETNA STRANICA")
+def o_nama(request):
+    return HttpResponse("OVO JE PRICA O NAMA")
